9052aa8bb64e3c301e208ce9f95aab699b637770
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-05-15 13:15:14 -0500

    He integrado un alert para la confirmación de cierre de sesión

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +9,35 @@ import { AuthService } from '../services/auth.service';
 })
 export class HomePage implements OnInit {
 
-  constructor( public authService : AuthService ) { }
+  constructor( public authService : AuthService,
+               public alertController: AlertController, ) { }
 
   ngOnInit() {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.presentAlert();
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Sesión',
+      subHeader: '¿Deseas cerrar sesión?',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.authService.logout();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
